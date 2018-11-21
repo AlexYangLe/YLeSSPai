47fb436c7b9f1e5478cf867bf514dce723c15630
AlexYang <alexyang@alexyangdemacbook-pro.local>
2018-11-21 13:46:55 +0800

    Initial Commit

---
+++ SSPai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSPai
+//
+//  Created by AlexYang on 2018/11/21.
+//  Copyright © 2018 AlexYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SSPai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSPai
+//
+//  Created by AlexYang on 2018/11/21.
+//  Copyright © 2018 AlexYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
